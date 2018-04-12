f7f2094ef718cdf3a29a17bd452927349b740556
Ryan Buchmayer <buchmr@uw.edu>
2018-04-11 20:50:09 -0700

    updated

--- cse331/src/hw3/test/BallContainerTest.java
+++ cse331/src/hw3/test/BallContainerTest.java
@@ -124,6 +124,7 @@ public class BallContainerTest {
     @Test
     public void testClear() {
         ballcontainer.clear();
+        System.out.println(ballcontainer.size()	);
         assertEquals("BallContainer is not empty after being cleared!",
                      0, ballcontainer.size());
         ballcontainer.add(b[0]);
